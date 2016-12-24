1575834c2d70e9402a72e80ace1a2ff5271b0a6e
Katharina Wittkowsky <katharina.wittkowsky@gmx.de>
2016-12-24 23:07:43 +0100

    eval is evil

--- calculator.js
+++ calculator.js
@@ -2,6 +2,6 @@ function calc() {
     var num1 = document.getElementById("nr1").value;
     var num2 = document.getElementById("nr2").value;
     var op = document.getElementById("operator").value;
-    var sum = eval(num1 + op + num2);
+    var sum = eval(num1 + op + num2); // i read eval is evil. i am searching for another option
     document.getElementById("summe").value = sum;
 }
