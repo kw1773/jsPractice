799252be052f96bd6c545385c26cb54df7836e37
Katharina Wittkowsky <katharina.wittkowsky@gmx.de>
2017-01-01 00:56:44 +0100

    add form to index.html get input

---
+++ form.js
@@ -0,0 +1,7 @@
+function data() {
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var telnr = document.getElementById("telnr").value;
+    
+    console.log(firstname + lastname + telnr);
+}
